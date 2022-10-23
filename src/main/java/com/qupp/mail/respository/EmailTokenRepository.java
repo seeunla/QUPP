62df19d4f83fbc3108562be0e92db821c03b4556
seeunla <107592967+seeunla@users.noreply.github.com>
2022-10-23 19:51:52 +0900

    Other: 프로젝트 가져오기

---
+++ src/main/java/com/qupp/mail/respository/EmailTokenRepository.java
@@ -0,0 +1,12 @@
+package com.qupp.mail.respository;
+
+import com.qupp.mail.dto.EmailToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface EmailTokenRepository extends JpaRepository<EmailToken, Long> {
+    Optional<EmailToken> findByVerificationToken(String token);
+}
